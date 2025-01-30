2ca2240eca74278a97660018a8898a51c9e4a60a
Bence Csókás <bence98@sch.bme.hu>
2025-01-30 09:35:27 +0100

    dissectors: Add simple Ogg dissector (`file-ogg`)
    It is currently capable of dissecting one OggS header
    without the segment table. It does not check CRC,
    nor does it handle payload data.

---
+++ epan/dissectors/file-ogg.c
@@ -0,0 +1,256 @@
+/* file-ogg.c
+ *
+ * Routines for audio/ogg media dissection
+ * Copyright 2025, Bence Csókás.
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * The Ogg specification is found at Xiph.org:
+ * https://xiph.org/ogg/doc/
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+#include "config.h"
+
+#include <epan/packet.h>
+#include <epan/expert.h>
+#include <wsutil/array.h>
+
+#include <wsutil/str_util.h>
+
+/** Size of the Ogg header without the segment table */
+#define OGG_HDR_LEN 27
+
+void proto_register_ogg(void);
+void proto_reg_handoff_ogg(void);
+
+static dissector_handle_t ogg_handle;
+
+static int proto_ogg;
+
+static int hf_magic;
+static int hf_version;
+static int hf_type;
+static int hf_position;
+static int hf_serial_no;
+static int hf_seq_no;
+static int hf_crc;
+static int hf_n_segs;
+
+static int hf_type_flags_cont;
+static int hf_type_flags_bos;
+static int hf_type_flags_eos;
+
+static int ett_ogg;
+static int ett_ogg_type;
+
+static expert_field ei_ogg_missing_magic;
+
+static unsigned
+find_ogg_page(tvbuff_t *tvb, unsigned offset, size_t len)
+{
+    bool found = false;
+
+    /* Find sync word */
+    while (offset < len - OGG_HDR_LEN) {
+        if (tvb_strneql(tvb, 0, "OggS", 4) == 0) {
+            found = true;
+            break;
+        }
+        offset++;
+    }
+
+    if (!found)
+        return len;
+
+    return offset;
+}
+
+static int
+dissect_ogg_page(tvbuff_t *tvb, proto_tree *tree, unsigned offset)
+{
+    uint8_t flags;
+    proto_item *ti, *ti_tree;
+    proto_tree *ogg_tree, *subtree;
+
+    ti_tree = proto_tree_add_item(tree, proto_ogg, tvb, offset, -1, ENC_NA);
+    ogg_tree = proto_item_add_subtree(ti_tree, ett_ogg);
+
+    proto_tree_add_item(ogg_tree, hf_magic,
+            tvb, offset, 4, ENC_ASCII|ENC_NA);
+    proto_tree_add_item(ogg_tree, hf_version,
+            tvb, offset + 4, 1, ENC_LITTLE_ENDIAN);
+
+    ti = proto_tree_add_item(ogg_tree, hf_type,
+            tvb, offset + 5, 1, ENC_LITTLE_ENDIAN);
+    subtree = proto_item_add_subtree(ti, ett_ogg_type);
+    flags = tvb_get_uint8(tvb, offset + 5);
+    proto_tree_add_boolean(subtree, hf_type_flags_cont, tvb, offset + 5, 1, flags);
+    proto_tree_add_boolean(subtree, hf_type_flags_bos , tvb, offset + 5, 1, flags);
+    proto_tree_add_boolean(subtree, hf_type_flags_eos , tvb, offset + 5, 1, flags);
+
+    proto_tree_add_item(ogg_tree, hf_position,
+            tvb, offset + 6, 8, ENC_LITTLE_ENDIAN);
+    proto_tree_add_item(ogg_tree, hf_serial_no,
+            tvb, offset + 14, 4, ENC_LITTLE_ENDIAN);
+    proto_tree_add_item(ogg_tree, hf_seq_no,
+            tvb, offset + 18, 4, ENC_LITTLE_ENDIAN);
+    proto_tree_add_item(ogg_tree, hf_crc,
+            tvb, offset + 22, 4, ENC_LITTLE_ENDIAN);
+    proto_tree_add_item(ogg_tree, hf_n_segs,
+            tvb, offset + 26, 1, ENC_LITTLE_ENDIAN);
+
+    offset += OGG_HDR_LEN;
+
+    proto_item_set_end(ti_tree, tvb, offset);
+
+    return offset;
+}
+
+static int
+dissect_ogg(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_)
+{
+    size_t len = tvb_reported_length(tvb);
+    unsigned offset = 0;
+
+    offset = find_ogg_page(tvb, offset, len);
+
+    if (offset > len - OGG_HDR_LEN - 1)
+        return 0;
+
+    col_set_str(pinfo->cinfo, COL_PROTOCOL, "Ogg");
+    col_clear(pinfo->cinfo, COL_INFO);
+
+    offset = dissect_ogg_page(tvb, tree, offset);
+
+    return offset;
+}
+
+static bool
+dissect_ogg_heur(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
+{
+    return dissect_ogg(tvb, pinfo, tree, data) > 0;
+}
+
+void
+proto_register_ogg(void)
+{
+    static hf_register_info hf[] = {
+        { &hf_magic,
+            { "Capture Pattern", "ogg.magic",
+              FT_STRING, BASE_NONE, NULL, 0x00,
+              "Ogg Stream Capture Pattern", HFILL }
+        },
+        { &hf_version,
+            { "Version", "ogg.version",
+              FT_UINT8, BASE_DEC, NULL, 0x00,
+              "Ogg Stream Structure Version", HFILL }
+        },
+        { &hf_type,
+            { "Type Flags", "ogg.type_flags",
+              FT_UINT8, BASE_HEX, NULL, 0x00,
+              "Page Header Type Flags", HFILL }
+        },
+        { &hf_type_flags_cont,
+            { "Continuation Flag",                                  "ogg.type_flags.cont",
+            FT_BOOLEAN, 8, NULL, 0x01,
+            "This packet is a continuation of the previous one.", HFILL }
+        },
+        { &hf_type_flags_bos,
+            { "Beginning of Stream",                                "ogg.type_flags.bos",
+            FT_BOOLEAN, 8, NULL, 0x02,
+            "First page of logical bitstream.", HFILL }
+        },
+        { &hf_type_flags_eos,
+            { "End of Stream",                                      "ogg.type_flags.eos",
+            FT_BOOLEAN, 8, NULL, 0x04,
+            "Last page of logical bitstream.", HFILL }
+        },
+        { &hf_position,
+            { "Absolute Granule Position", "ogg.position",
+              FT_UINT64, BASE_DEC, NULL, 0x00,
+              "Total samples encoded after including all packets finished on this page.", HFILL }
+        },
+        { &hf_serial_no,
+            { "Serial Number", "ogg.serial_no",
+              FT_UINT32, BASE_HEX, NULL, 0x00,
+              "Logical bitstream identifier", HFILL }
+        },
+        { &hf_seq_no,
+            { "Sequence Number", "ogg.seq_no",
+              FT_UINT32, BASE_DEC, NULL, 0x00,
+              "Page counter", HFILL }
+        },
+        { &hf_crc,
+            { "CRC32", "ogg.crc32",
+              FT_UINT32, BASE_HEX, NULL, 0x00,
+              "Page Checksum", HFILL }
+        },
+        { &hf_n_segs,
+            { "Segments", "ogg.segment_table.len",
+              FT_UINT8, BASE_DEC, NULL, 0x00,
+              "Ogg Stream Structure Version", HFILL }
+        },
+    };
+
+    /* Setup protocol subtree array */
+    static int *ett[] = {
+        &ett_ogg,
+        &ett_ogg_type,
+    };
+
+    static ei_register_info ei[] = {
+        { &ei_ogg_missing_magic,
+            { "ogg.magic.missing", PI_PROTOCOL, PI_ERROR,
+                "Capture Pattern not found!", EXPFILL }
+        }
+    };
+
+    expert_module_t* expert_ogg;
+
+    /* Register the protocol name and description */
+    proto_ogg = proto_register_protocol(
+            "Xiph.org Ogg Stream",
+            "Ogg",
+            "ogg"
+    );
+
+    /* Required function calls to register the header fields
+     * and subtrees used */
+    proto_register_field_array(proto_ogg, hf, array_length(hf));
+    proto_register_subtree_array(ett, array_length(ett));
+    expert_ogg = expert_register_protocol(proto_ogg);
+    expert_register_field_array(expert_ogg, ei, array_length(ei));
+
+    ogg_handle = register_dissector("ogg", dissect_ogg, proto_ogg);
+}
+
+void
+proto_reg_handoff_ogg(void)
+{
+    dissector_add_string("media_type", "audio/ogg", ogg_handle);
+    dissector_add_string("media_type", "video/ogg", ogg_handle);
+    dissector_add_string("media_type", "application/ogg", ogg_handle);
+    dissector_add_string("media_type", "audio/x-vorbis+ogg", ogg_handle);
+    dissector_add_string("media_type", "audio/x-opus+ogg", ogg_handle);
+    dissector_add_string("media_type", "video/theora+ogg", ogg_handle);
+
+    heur_dissector_add("http", dissect_ogg_heur, "Ogg file in HTTP", "ogg_http", proto_ogg, HEURISTIC_ENABLE);
+    heur_dissector_add("wtap_file", dissect_ogg_heur, "Ogg file", "ogg_wtap", proto_ogg, HEURISTIC_ENABLE);
+}
+
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
